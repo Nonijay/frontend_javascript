1397a40132d59f5d0e44e1dc86b45a871b24b90f
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-25 09:34:07 -0500

    Update

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,24 @@
+const teacher3: object = {
+    firstName: 'Chinonso',
+    lastName: 'Chukwu' ,
+    fullTimeEmployee: true,
+    yearsOfExperience: 3,
+    location: 'Lagos',
+    contract: true,
+}
+
+console.log(teacher3)
+
+const directorS: object = {
+
+    firstName: 'Chinonso',
+    lastName: 'Chukwu',
+    location: 'Lagos',
+    fullTimeEmployee: true,
+    yearsOfExperience: 5,
+    contract: true,
+    numberOfReports: 50,
+}
+
+
+
