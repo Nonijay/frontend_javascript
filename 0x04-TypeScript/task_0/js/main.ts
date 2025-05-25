f2f0a6ce136588bd37cba5076440373ac598d8ca
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-25 10:12:17 -0500

    commit task

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,24 +1,29 @@
-const teacher3: object = {
-    firstName: 'Chinonso',
-    lastName: 'Chukwu' ,
-    fullTimeEmployee: true,
-    yearsOfExperience: 3,
-    location: 'Lagos',
-    contract: true,
-}
+interface student { 
+        firstName: string;
+        lastName: string;
+        age: number;
+        location: string;
+    }
 
-console.log(teacher3);
+//create a variable named firstStudent with the type student
+//create a variable named secondStudent with the type student
+const firstStudent: student = {
+    firstName: "Jeff",
+    lastName: "Chukwu",
+    age: 28,
+    location: "Lagos",
+} ;
+const secondStudent: student = {
+    firstName: "Chiedozie",
+    lastName: "Chukwu",
+    age: 30,
+    location: "Lagos",
+};
 
-const directorS: object = {
+//Create two students, and create an array named studentsList containing the two variables
 
-    firstName: 'Chinonso',
-    lastName: 'Chukwu',
-    location: 'Lagos',
-    fullTimeEmployee: true,
-    yearsOfExperience: 5,
-    contract: true,
-    numberOfReports: 50,
-}
+const studentsList: student[] =[firstStudent, secondStudent];
+//using vanilla JS render a table for each elemnts in the array, append a new row to the table.
 
-console.log(directorS);
-
+const tbody = document.createElement('tbody');
+tbody.id = 'myTableBody';
