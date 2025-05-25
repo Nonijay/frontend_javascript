c0625baaacbfe5840865b61861e7e625a1a1d120
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-25 15:31:57 -0500

    new commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -34,4 +34,26 @@ printTeacher("Chinonso","Chukwu");
 function printTeacher(firstName: string, lastName: string) {
     return `${firstName[0]}.${lastName}`;
 }
-
+// Create Contsructor that accepts firstName(string), lastName(string), method workOnHome, method displayName
+class StudentClass {
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        workOnHome: () => {
+            console.log(" is diligently working on their home improvements");
+        }
+        displayName: () => {
+            console.log(`My name is ${this.firstName} ${this.lastName}`);
+        }
+    }
+//The constructor of the class should be described through an Interface
+//The class should be described through an Interface
+    firstName: string;
+    lastName: string;
+    workOnHome: () => {
+    console.log("Working on homework");
+    }
+    displayName: () => {
+        console.log(`My name is ${this.firstName} ${this.lastName}`);
+    }
+}
